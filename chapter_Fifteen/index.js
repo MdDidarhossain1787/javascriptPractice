4ffa0f45ac6931427eba3ca7166cfcfce9567d8f
MdDidarhossain1787 <mddidarhossain8771@gmail.com>
2023-11-20 22:28:57 +0600

    Class and Class Proparties and Static Method

--- chapter_Fifteen/index.js
+++ chapter_Fifteen/index.js
@@ -373,7 +373,7 @@
 
 //  Weak Set 
 
-let a ={a: 10}, b ={b: 20}
+// let a ={a: 10}, b ={b: 20}
 
 // let set = new Set([a , b])
 // a = null
@@ -390,7 +390,67 @@ let a ={a: 10}, b ={b: 20}
 
 // Weak Map
 
-let weakMap = new WeakMap([[a, 45] , [b, 75]])
+// let weakMap = new WeakMap([[a, 45] , [b, 75]])
 // a = null
 
-console.log(weakMap.get(a))
+// console.log(weakMap.get(a))
+
+// function Rectangle (width, height){
+//     this.width = width
+//     this.height = height
+// }
+
+// Rectangle.prototype.draw = function(){
+//     console.log(this)
+//     console.log('Drawing.....')
+// }
+
+// let rect1 = new Rectangle(12, 15)
+
+// class Rectangle {
+//     constructor(width , height){
+//         this.width = width
+//         this.height = height
+//     }
+// name = 'HM Nayem'
+// test2 = function(){
+
+// }
+
+//     draw(){
+//         console.log('Drawing...')
+//     }
+
+//     test (){
+
+//     }
+// }
+
+// let rect1 = new Rectangle(45, 23)
+
+// console.log(rect1)
+// console.log(rect1)
+
+
+class Person{
+    constructor(name, email){
+        this.name = name
+        this.email = email
+    }
+
+    print(){
+        console.log(this.name, this.email)
+    }
+
+    static create(){
+        let person = JSON.parse(str)
+        return new Person(person.name, person.email)
+    }
+}
+
+let str = '{"name":"HM Nayem","email":"hasan.m.nayem"}'
+
+let p1 = Person.create(str)
+console.log(p1)
+console.log(p1 instanceof Person)
+p1.print()
